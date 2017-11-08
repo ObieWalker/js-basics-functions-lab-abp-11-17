9b47e7bc44fd0d904163b298ed1d80655a832616
Obi <obinnawalker@gmail.com>
2017-11-08 13:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks () {
- 
+function distanceFromHqInBlocks (location, hq) {
+  return hq -location
+
 }
- 
+
 function distanceFromHqInFeet () {
   distanceFromHqInBlocks();
   // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function
